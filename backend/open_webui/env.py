c67fa2346fb7bc1f2ae822d66fd34d6263ef42b6
dhliu <dhliu@yoozoo.com>
2025-04-28 15:12:46 +0800

    测试

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -105,9 +105,13 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+# WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
+# if WEBUI_NAME != "Open WebUI":
+#     WEBUI_NAME += " (Open WebUI)"
+
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "YOOZOO AI")
+if WEBUI_NAME != "YOOZOO AI":
+    WEBUI_NAME += " (YOOZOO AI)"
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
